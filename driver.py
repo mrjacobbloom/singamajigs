69e38a8adc5e578a7c285eceee179a38482b5032
Jacob Bloom <mr.jacob.bloom@gmail.com>
2018-03-23 03:45:06 -0600

    looks like it's still pretty borken :/

--- driver.py
+++ driver.py
@@ -1,6 +1,6 @@
 from singamajigs import singamajigs
 from pprint import pprint
 
-test_melody = ["a4", "a4", "a4"]
+test_melody = ["c4", "d4", "db4"]
 score = singamajigs(test_melody)
 pprint(score)
--- singamajigs.py
+++ singamajigs.py
@@ -2,18 +2,20 @@ import json
 import random
 import copy
 
-class _Globals:
-    # enum the possibility types
-    POSSTYPES = {
-        'NEXT': 0, # play an existing jig
-        'NEW': 1   # add a new one
-    }
+class _NoteInstance:
+    def __init__(self, songIndex, noteIndex, noteObj):
+        self.songIndex = songIndex
+        self.noteIndex = noteIndex
+        self.noteObj = noteObj
+        self.pitch = noteObj['pitch']
+        
 
-    # enum the costs as well
+class _Globals:
+    # enum the costs
     COSTS = {
         'SAME': 0, # same jig plays multiple notes in a row
         'DIFF': 1, # different jig already in set plays the next note
-        'ADD':  3, # add a new jig to the set
+        'ADD':  6, # add a new jig to the set
         'OCT':  2  # extra cost incurred if the note is in the wrong octave
     }
     
@@ -34,19 +36,14 @@ class _Globals:
     singamajig_data = json.load(open('singamajig_data.json'))
     # database of all the notes in all the songs, categorized by note name
     songs_by_note = {}
-    for songIndex in range(0, len(singamajig_data)):
+    for songIndex in xrange(0, len(singamajig_data)):
         notes = singamajig_data[songIndex]['notes']
-        for noteIndex in range(0, len(notes)):
+        for noteIndex in xrange(0, len(notes)):
             noteObj = notes[noteIndex]
-            note = noteObj['note']
+            note = noteObj['pitch']
             if not note in songs_by_note:
                 songs_by_note[note] = []
-            songs_by_note[note].append({
-                'songIndex': songIndex,
-                'noteIndex': noteIndex,
-                'octave': noteObj['octave'],
-                'noteObject': noteObj
-            })
+            songs_by_note[note].append(_NoteInstance(songIndex, noteIndex, noteObj))
 
 class _Singamajig:
     _globals = _Globals()
@@ -54,10 +51,9 @@ class _Singamajig:
         self.songIndex = songIndex
         self.songData = self._globals.singamajig_data[songIndex]
         self.notes = self.songData['notes']
-        self.skin = random.choice(self.songData['skins'])
     
     def getNextIndex(self, prevIndex):
-        return prevIndex % len(self.notes)
+        return (prevIndex + 1) % len(self.notes)
     
     def getNote(self, noteNum):
         noteNum = noteNum % len(self.notes)
@@ -77,10 +73,10 @@ class _ScoreState:
         self.noteObj = self.song['notes'][self.noteIndex]
     def __str__(self):
         return "\"%s\" [id %d] - %d:%s%d \"%s\"" % (
-            self.song['song'],
+            self.song['title'],
             self.singamajigIndex,
             self.noteIndex,
-            self.noteObj['note'],
+            self.noteObj['pitch'],
             self.noteObj['octave'],
             self.noteObj['syllable']
         )
@@ -110,22 +106,23 @@ class _Possibility:
     def add(self, jig, noteIndex):
         self.currentJig = len(self.states)
         self.states.append(_State(jig, len(self.states), noteIndex))
+        print(jig.getNote(noteIndex)['pitch'])
         self.cost += self._globals.COSTS['ADD']
     
     def advance(self, jigIndex):
         state = self.states[jigIndex]
-        state.noteIndex = state.singamajig.getNextIndex()
+        state.noteIndex = state.singamajig.getNextIndex(state.noteIndex)
         if jigIndex == self.currentJig:
-            self.cost += _globals.COSTS['SAME']
+            self.cost += self._globals.COSTS['SAME']
         else:
-            self.cost += _globals.COSTS['DIFF']
+            self.cost += self._globals.COSTS['DIFF']
             self.currentJig = jigIndex
     
     def getAdvanceableJigIndexes(self, note):
         matchingJigs = []
         for state in self.states:
-            nextNote = state
-            if state.singamajig.getNote(state.noteIndex + 1) == note:
+            nextNote = state.singamajig.getNote(state.noteIndex + 1)['pitch']
+            if nextNote == note:
                 matchingJigs.append(state.singamajigIndex)
         return matchingJigs
     
@@ -142,7 +139,7 @@ def singamajigs(melody):
     # instantiate one SIngamajig object per song
     # (since they don't track their own state it's fine lol)
     allSingamajigs = []
-    for songIndex in range(0, len(_globals.singamajig_data)):
+    for songIndex in xrange(0, len(_globals.singamajig_data)):
         allSingamajigs.append(_Singamajig(songIndex))
     
     for melody_note in melody:
@@ -156,23 +153,25 @@ def singamajigs(melody):
         # catch it now if there are no singamajigs that sing the note
         if not melody_pitch in _globals.songs_by_note:
             return None
-        
         for prevPoss in prevPossibilities:
             nexts = prevPoss.getAdvanceableJigIndexes(melody_pitch)
             for nextJig in nexts:
                 newPoss = copy.deepcopy(prevPoss)
+                newPoss.previousPoss = prevPoss
                 newPoss.advance(nextJig)
                 possibilities.append(newPoss)
             for noteInstance in _globals.songs_by_note[melody_pitch]:
                 newPoss = copy.deepcopy(prevPoss)
-                jig = allSingamajigs[noteInstance['songIndex']]
-                newPoss.add(jig, noteInstance['noteIndex'])
+                newPoss.previousPoss = prevPoss
+                jig = allSingamajigs[noteInstance.songIndex]
+                newPoss.add(jig, noteInstance.noteIndex)
                 possibilities.append(newPoss)
         
         # ok now it's beam time
         # select the cheapest possibilities before moving on to the next round
-        possibilities.sort(key = _globals.sort)
+        possibilities.sort(key=_globals.sort)
         prevPossibilities = possibilities[:_globals.BEAM_WIDTH]
+        for poss in prevPossibilities: print(poss.cost);
     if(len(prevPossibilities) == 0):
         return None
     
