6a9c355284c156b7fdc3ab3ae1737a9ee8763d8a
Jacob Bloom <mr.jacob.bloom@gmail.com>
2018-03-23 01:34:33 -0600

    sort of got something to run without crashing sometimes!

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # singamajigs
 This algorithm produces a score to perform a given melody on a number of
-Sing-a-ma-jigs while optimizing for the smallest set of dolls needed.
+Sing-a-ma-jigs. It optimizes for most notes in a row, then smallest set of dolls
+needed. It doesn't necessarily find the best possible solution because the
+number of possibilities increases exponentially with every note. Instead, I used
+a [beam search](https://en.wikipedia.org/wiki/Beam_search) which saves some
+fixed number of the most promising solutions, which makes it partially resilient
+to local maxima but won't take up a ludicrous amount of memory either.
 
 ```shell
 python driver.py
@@ -15,7 +20,8 @@ theoretically use many Sing-a-ma-jigs to play a different melody of their
 choosing. Imagine a handbell choir where every time you play a note that bell
 will play a predictable but different note the next time. This algorithm
 (assuming I can get it to work) outputs a score to play a song under those
-constraints, while optimizing to minimize the number of Sing-a-ma-jigs (bells)
+constraints, while optimizing to maximize the number of consecutive notes played
+by one Sing-a-ma-jig (bell) and then minimize the number of Sing-a-ma-jigs
 needed.
 
 ## Why?
--- singamajigs.py
+++ singamajigs.py
@@ -1,13 +1,187 @@
 import json
+import random
+import copy
 
-singamajig_data = json.load(open('singamajig_data.json'))
+class _Globals:
+    # enum the possibility types
+    POSSTYPES = {
+        'NEXT': 0, # play an existing jig
+        'NEW': 1   # add a new one
+    }
+
+    # enum the costs as well
+    COSTS = {
+        'SAME': 0, # same jig plays multiple notes in a row
+        'DIFF': 1, # different jig already in set plays the next note
+        'ADD':  3, # add a new jig to the set
+        'OCT':  2  # extra cost incurred if the note is in the wrong octave
+    }
+    
+    SHARPS = {
+        'c#': 'db',
+        'd#': 'eb',
+        'e#': 'f', # trust no one
+        'f#': 'gb',
+        'g#': 'ab',
+        'a#': 'bb',
+        'b#': 'c'
+    }
+    
+    def sort(self, poss):
+        return poss.cost
+
+    BEAM_WIDTH = 10 # number of best possibilities to keep each round
+    singamajig_data = json.load(open('singamajig_data.json'))
+    # database of all the notes in all the songs, categorized by note name
+    songs_by_note = {}
+    for songIndex in range(0, len(singamajig_data)):
+        notes = singamajig_data[songIndex]['notes']
+        for noteIndex in range(0, len(notes)):
+            noteObj = notes[noteIndex]
+            note = noteObj['note']
+            if not note in songs_by_note:
+                songs_by_note[note] = []
+            songs_by_note[note].append({
+                'songIndex': songIndex,
+                'noteIndex': noteIndex,
+                'octave': noteObj['octave'],
+                'noteObject': noteObj
+            })
+
+class _Singamajig:
+    _globals = _Globals()
+    def __init__(self, songIndex):
+        self.songIndex = songIndex
+        self.songData = self._globals.singamajig_data[songIndex]
+        self.notes = self.songData['notes']
+        self.skin = random.choice(self.songData['skins'])
+    
+    def getNextIndex(self, prevIndex):
+        return prevIndex % len(self.notes)
+    
+    def getNote(self, noteNum):
+        noteNum = noteNum % len(self.notes)
+        return self.notes[noteNum]
+    
+    def __deepcopy__(self, memo): # override this so they never get duplicated
+        memo[id(self)] = self
+        return self
+
+class _ScoreState:
+    _globals = _Globals()
+    def __init__(self, singamajigIndex, songIndex, noteIndex):
+        self.singamajigIndex = singamajigIndex
+        self.songIndex = songIndex
+        self.noteIndex = noteIndex
+        self.song = self._globals.singamajig_data[self.songIndex]
+        self.noteObj = self.song['notes'][self.noteIndex]
+    def __str__(self):
+        return "\"%s\" [id %d] - %d:%s%d \"%s\"" % (
+            self.song['song'],
+            self.singamajigIndex,
+            self.noteIndex,
+            self.noteObj['note'],
+            self.noteObj['octave'],
+            self.noteObj['syllable']
+        )
+
+class _State:
+    def __init__(self, singamajig, singamajigIndex, noteIndex):
+        self.singamajig = singamajig
+        self.singamajigIndex = singamajigIndex
+        self.noteIndex = noteIndex
+        
+    def toScoreState(self):
+        return _ScoreState(
+            self.singamajigIndex,
+            self.singamajig.songIndex,
+            self.noteIndex
+        )
+
+class _Possibility:
+    _globals = _Globals()
+    # store a reference to the previous possibility instead of duplicating everything?
+    
+    states = [] # a list of state dicts: (singamajig, noteIndex)
+    cost = 0 # cost of previous possibility plus next possibility
+    currentJig = None # the Singamajig that plays the note
+    previousPoss = None # so we can reconstruct the score when done
+    
+    def add(self, jig, noteIndex):
+        self.currentJig = len(self.states)
+        self.states.append(_State(jig, len(self.states), noteIndex))
+        self.cost += self._globals.COSTS['ADD']
+    
+    def advance(self, jigIndex):
+        state = self.states[jigIndex]
+        state.noteIndex = state.singamajig.getNextIndex()
+        if jigIndex == self.currentJig:
+            self.cost += _globals.COSTS['SAME']
+        else:
+            self.cost += _globals.COSTS['DIFF']
+            self.currentJig = jigIndex
+    
+    def getAdvanceableJigIndexes(self, note):
+        matchingJigs = []
+        for state in self.states:
+            nextNote = state
+            if state.singamajig.getNote(state.noteIndex + 1) == note:
+                matchingJigs.append(state.singamajigIndex)
+        return matchingJigs
+    
+    def getScoreState(self):
+        if not self.currentJig == None:
+            return self.states[self.currentJig].toScoreState()
+        else:
+            return None
 
 def singamajigs(melody):
-    possibilities = []
+    _globals = _Globals()
+    prevPossibilities = [_Possibility()]
+    
+    # instantiate one SIngamajig object per song
+    # (since they don't track their own state it's fine lol)
+    allSingamajigs = []
+    for songIndex in range(0, len(_globals.singamajig_data)):
+        allSingamajigs.append(_Singamajig(songIndex))
+    
     for melody_note in melody:
-        if "#" in melody_note:
-            "bleb"
-            # @TODO figure out how to flat the sharp things
-        # @TODO ok so how the heck is this actually gonna work
-        possibilities.append(melody_note + " is a bad note")
-    return possibilities;
+        possibilities = [] # possibilities for the current round
+        melody_note = melody_note.lower()
+        melody_pitch = melody_note[:-1]
+        melody_octave = melody_note[-1]
+        if '#' in melody_pitch and melody_pitch in _globals.SHARPS:
+            melody_pitch = _globals.SHARPS[melody_pitch]
+        
+        # catch it now if there are no singamajigs that sing the note
+        if not melody_pitch in _globals.songs_by_note:
+            return None
+        
+        for prevPoss in prevPossibilities:
+            nexts = prevPoss.getAdvanceableJigIndexes(melody_pitch)
+            for nextJig in nexts:
+                newPoss = copy.deepcopy(prevPoss)
+                newPoss.advance(nextJig)
+                possibilities.append(newPoss)
+            for noteInstance in _globals.songs_by_note[melody_pitch]:
+                newPoss = copy.deepcopy(prevPoss)
+                jig = allSingamajigs[noteInstance['songIndex']]
+                newPoss.add(jig, noteInstance['noteIndex'])
+                possibilities.append(newPoss)
+        
+        # ok now it's beam time
+        # select the cheapest possibilities before moving on to the next round
+        possibilities.sort(key = _globals.sort)
+        prevPossibilities = possibilities[:_globals.BEAM_WIDTH]
+    if(len(prevPossibilities) == 0):
+        return None
+    
+    currentPossibility = prevPossibilities[0]
+    # reconstruct the possibility into a consumable format
+    score = []
+    while not currentPossibility == None:
+        scoreState = currentPossibility.getScoreState()
+        if not scoreState == None:
+            score.insert(0, str(scoreState))
+        currentPossibility = currentPossibility.previousPoss
+    return score
